cec5779657fff422b94bef48a7132e6425fa5f24
NorvilzShawnz <NorvilzShawnz@gmail.com>
2026-03-12 15:19:35 -0400

    more refactoring and api endpoint changes to conceal usage even further

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-const apiKey = process.env.REACT_APP_NUMVERIFY_API_KEY;
+
 
 export const validatePhoneNumber = async (phoneNumber) => {
   const fullNumber = `1${phoneNumber}`;
 
   const response = await fetch(
-    `${process.env.REACT_APP_NUMVERIFY_API_URL}?access_key=${apiKey}&number=${fullNumber}`
+    `${process.env.REACT_APP_VERIFY_API_URL}?access_key=${process.env.REACT_APP_VERIFY_API_KEY}&number=${fullNumber}`
   );
 
   const data = await response.json();
